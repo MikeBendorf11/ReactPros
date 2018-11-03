3824626e177dbf5568da1c9315c3f9ba1b07719f
MikeB <mikebendorf11@gmail.com>
2018-11-02 20:11:12 -0700

    Components call other components

--- src/index.js
+++ src/index.js
@@ -4,9 +4,72 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo author={props.author}/>
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+
+function Avatar(props) {
+  return (
+    <img
+      className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  )
+}
+
+function UserInfo(props){
+  return(
+    <div className="UserInfo">
+      <Avatar user={props.author} />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+  )
+}
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'https://placekitten.com/g/64/64',
+  },
+};
+
+ReactDOM.render(
+  <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author}
+  />,
+  document.getElementById('root')
+);
+
+
+
+
+
+
+
+
+
+
 serviceWorker.unregister();
+
+
+//https://reactjs.org/docs/rendering-elements.html
