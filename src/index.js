d9ad6be5cc9f6adc62482f88e2500480c3f58129
MikeB <mikebendorf11@gmail.com>
2018-11-06 13:08:00 -0800

    2 ways of handling events, button disables mouseover though

--- src/index.js
+++ src/index.js
@@ -6,37 +6,47 @@ import * as serviceWorker from './serviceWorker';
 
 var a = 1;
 
-//can't be called within class purpose??
 function AnotherTag(props){
   return <h1>{props.num}</h1>
 }
+//one way
+function Abutton(props){
+  return <button onClick={()=>clearInterval(props.itv)}>A Button</button>
 
+}
 
 class Atag extends React.Component{
-  //same as function Atag(){} but can set state and constructor
   constructor(props){
     super(props)
+    this.anotherNumb = 0;
     this.state = {
       b:0,
-      c: ''
+      c: '',
     }
+    //add a function to an event
+    this.handleMouseOver = this.handleMouseOver.bind(this);
   }
+  //second way
+  handleMouseOver(){
+    this.anotherNumb = this.anotherNumb + 1;
+  }
+  //this is why we would use state, to track var with events
   componentDidMount(){
     this.itv = setInterval(()=>{
       if((this.state.b)%2 == 0){
         this.setState({c: 'asd'+this.state.b})
       }
-      //setState can be self assignment
       this.setState({b: this.state.b+1})
     },1000);
-   
   }
   
   render(){
     return(
       <div>
+        <p onMouseOver={this.handleMouseOver}>{this.anotherNumb}</p>
         <AnotherTag num={this.state.c}/>
         <p>HI! {this.state.b}</p>
+        <Abutton itv={this.itv}/>
       </div>
       
     )
