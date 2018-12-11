e3a3eb99147c567c1d383ccafcfcfdf7e0da7a95
MikeB <mikebendorf11@gmail.com>
2018-12-11 10:56:41 -0800

    not displaying

--- src/index.js
+++ src/index.js
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import car from './cars';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function CarList(props){
+  const list = props.car;
+  const aList = (
+    list.forEach(l => 
+      <li key={l.brand}>
+      {l.brand}
+    </li>
+  ));
+       
+  return(
+    <ul>
+      {aList}
+    </ul>
+  )
+}
+const cars = car;
+ReactDOM.render(<CarList car={cars}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
