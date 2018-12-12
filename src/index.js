16ce5d950c29d46d4c2055e5193a4c827eca1f51
MikeB <mikebendorf11@gmail.com>
2018-12-12 10:11:57 -0800

    2 Render divs

--- src/index.js
+++ src/index.js
@@ -41,7 +41,8 @@ function CarList(props){
 const cars = car;
 //console.log(cars);
 ReactDOM.render(<CarList car={cars}/>, document.getElementById('root'));
-
+ReactDOM.render(<CarList car={cars}/>, document.getElementById('root2'));
+ 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
